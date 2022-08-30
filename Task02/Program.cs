9adcae44c49216adacbe19bf469351eb0b8ebbee
bonew1988 <bonew@bonew.ru>
2022-08-30 22:39:51 +0300

    end seminar 30.08.2022

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -6,12 +6,12 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 if(num1 < 8 && num1 > 0)
 {
     if(num1 == 1) Console.WriteLine("Понедельник ");
-    if(num1 == 2) Console.WriteLine("Вторник ");
-    if(num1 == 3) Console.WriteLine("Среда ");
-    if(num1 == 4) Console.WriteLine("Четверг ");
-    if(num1 == 5) Console.WriteLine("Пятница ");
-    if(num1 == 6) Console.WriteLine("Суббота ");
-    if(num1 == 7) Console.WriteLine("Воскресенье ");
+    else if(num1 == 2) Console.WriteLine("Вторник ");
+    else if(num1 == 3) Console.WriteLine("Среда ");
+    else if(num1 == 4) Console.WriteLine("Четверг ");
+    else if(num1 == 5) Console.WriteLine("Пятница ");
+    else if(num1 == 6) Console.WriteLine("Суббота ");
+    else if(num1 == 7) Console.WriteLine("Воскресенье ");
 }
 else 
 {
