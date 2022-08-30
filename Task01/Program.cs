bb3a9e0c88adb6d2d723b8920ad3f223972b6adf
bonew1988 <bonew@bonew.ru>
2022-08-30 22:00:27 +0300

    add task02

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Является ли первое число квадратом второго?
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num2 * num2 == num1)
+{
+    Console.WriteLine($"ДА");
+}
+else
+    Console.WriteLine($"НЕТ");
---
+++ Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿//день недели по номеру дня
+
+Console.Clear();
+Console.Write("Ведите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1 < 8 && num1 > 0)
+{
+    if(num1 == 1) Console.WriteLine("Понедельник ");
+    if(num1 == 2) Console.WriteLine("Вторник ");
+    if(num1 == 3) Console.WriteLine("Среда ");
+    if(num1 == 4) Console.WriteLine("Четверг ");
+    if(num1 == 5) Console.WriteLine("Пятница ");
+    if(num1 == 6) Console.WriteLine("Суббота ");
+    if(num1 == 7) Console.WriteLine("Воскресенье ");
+}
+else 
+{
+    Console.WriteLine("Ошибка, введите число от 1 до 7");
+}
