5698d809ecbd60ba88476bfc82e06c77b9cba2f5
bonew1988 <bonew@bonew.ru>
2022-09-01 23:30:17 +0300

    add second homework

--- HomeWorks/Task002/Program.cs
+++ HomeWorks/Task002/Program.cs
@@ -6,15 +6,21 @@
 
 Console.Clear();
 Console.WriteLine("Автор: Новиков ВВ");
+Console.WriteLine("-----------------");
 Console.Write("Введите число #1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число #2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int max = 0;
+int min = 0;
 if (num1 > num2)
 {
     max = num1;
+    min = num2;
 }
 else max = num2;
+    min = num1;
 
+Console.WriteLine("-----------------");
 Console.WriteLine($"Максимальное значение: {max}");
+Console.WriteLine($"Минимальное значение: {min}");
---
+++ HomeWorks/Task004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Автор: Новиков ВВ");
+Console.WriteLine("-----------------");
+Console.Write("Введите число #1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число #2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число #3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine("-----------------");
+Console.WriteLine($"Максимальное значение: {max}");
