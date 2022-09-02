aaa63608dd102d4120588f509f2a43e8d66e304e
bonew1988 <bonew@bonew.ru>
2022-09-02 21:36:06 +0300

    add tasks 05. 06. 07

---
+++ Task05/Program.cs
@@ -0,0 +1,36 @@
+﻿// случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа
+// 78->8
+// 12->2
+// 85->8
+//-------------------------------------------------------------------------------
+
+int number = new Random().Next(10, 100);
+
+//int number = 55;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+//          первый вариант решения
+// if(firstDigit > secondDigit) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
+// else System.Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
+
+//          Решение через Тернарный оператор
+
+// if (firstDigit == secondDigit) Console.WriteLine($"Цифры рывны");
+// else
+// {
+//     int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+//     System.Console.WriteLine($"Наибольшая цифра числа {number} -> {max}");
+// }
+
+//          Решение через Метод
+
+int MaxDigit(int num)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int maxDigit = MaxDigit(number);
+System.Console.WriteLine($"Наибольшая цифра числа {number} -> {maxDigit}");
