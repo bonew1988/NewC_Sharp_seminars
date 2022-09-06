67194e31bf55faa226ef11cb6090b7289c6c37bb
bonew1988 <bonew@bonew.ru>
2022-09-06 20:49:31 +0300

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+//-------------------------------------------------------------
+
+
+
+Console.Clear();
+Console.Write("Введите номер четверти (1-4) ");
+Console.Write(":");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int quart1)
+{
+    if (quart1 == 1) return "xc > 0 && yc > 0";
+    if (quart1 == 2) return "xc < 0 && yc > 0";
+    if (quart1 == 3) return "xc < 0 && yc < 0";
+    if (quart1 == 4) return "xc > 0 && yc < 0";
+    return "Введен некорректный номер четверти";
+}
+
+string result = Quarter(quart);
+Console.WriteLine(result);
