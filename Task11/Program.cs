c88a7d9074a13165aa8755982ba5e305ddbdef4f
bonew1988 <bonew@bonew.ru>
2022-09-06 21:31:10 +0300

    add Task12

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -4,18 +4,16 @@
 //-------------------------------------------------------------
 
 
-
 Console.Clear();
-Console.Write("Введите номер четверти (1-4) ");
-Console.Write(":");
-int quart = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер четверти (1-4): ");
+string quart = Console.ReadLine();
 
-string Quarter(int quart1)
+string Quarter(string quart1)
 {
-    if (quart1 == 1) return "xc > 0 && yc > 0";
-    if (quart1 == 2) return "xc < 0 && yc > 0";
-    if (quart1 == 3) return "xc < 0 && yc < 0";
-    if (quart1 == 4) return "xc > 0 && yc < 0";
+    if (quart1 == "1") return "xc > 0 ; yc > 0";
+    if (quart1 == "2") return "xc < 0 ; yc > 0";
+    if (quart1 == "3") return "xc < 0 ; yc < 0";
+    if (quart1 == "4") return "xc > 0 ; yc < 0";
     return "Введен некорректный номер четверти";
 }
 
---
+++ Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+//-------------------------------------------------------------
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки A ");
+Console.Write("x:");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("y:");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+Console.Write("x:");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("y:");
+double by = Convert.ToDouble(Console.ReadLine());
+
+double Result(double ax1, double ay1, double bx1, double by1)
+{
+    return Math.Sqrt((ax1 - bx1) * (ax1 - bx1) + (ay1 - by1) * (ay1 - by1));
+}
+double result = Result(ax, ay, bx, by);
+Console.WriteLine(Math.Round(result, 2));
