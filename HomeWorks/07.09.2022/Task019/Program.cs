64ffd8199d95310b41f6ae280aaec2f19f848509
bonew1988 <bonew@bonew.ru>
2022-09-07 17:44:06 +0300

    add Task19 Homework

---
+++ HomeWorks/07.09.2022/Task019/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+//-------------------------------------------------------------
+
+Console.Clear();
+Console.WriteLine("Автор: Новиков ВВ");
+Console.WriteLine("-----------------");
+Console.WriteLine();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("-----------------");
+int Module(int num)
+{
+    if (num < 0)
+    {
+        return -num;
+    }
+    else return num;
+}
+int result = Module(number);
+void Reverse(int num1)
+{
+    if (num1 / 10000 == num1 % 10 && num1 % 100 / 10 == num1 % 10000 / 1000)
+    {
+        Console.WriteLine($"Палиндром");
+    }
+    else Console.WriteLine($"НЕ палиндром");
+}
+if (result > 9999 & result <= 99999)
+{
+    Reverse(result);
+}
+else Console.WriteLine($"Введено некорректное число");
