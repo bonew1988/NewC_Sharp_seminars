4e3719128ead426217d4cf41d1e8b189a24e1488
bonew1988 <bonew@bonew.ru>
2022-09-07 18:12:35 +0300

    add 07.09.2022  Homework done!

---
+++ HomeWorks/07.09.2022/Task021/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+//-------------------------------------------------------------------------
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("x:");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("y:");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("z:");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("x:");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("y:");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("z:");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+double Result(double ax1, double ay1, double az1, double bx1, double by1, double bz1)
+{
+    return Math.Sqrt((ax1 - bx1) * (ax1 - bx1) + (ay1 - by1) * (ay1 - by1) + (az1 - bz1) * (az1 - bz1));
+}
+double result = Result(ax, ay, az, bx, by, bz);
+Console.WriteLine($"Расстояние между точками составит: {Math.Round(result, 2, MidpointRounding.ToZero)}");
---
+++ HomeWorks/07.09.2022/Task023/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+//-------------------------------------------------------
+Console.Clear();
+Console.Write("Введите положительное целое число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+void Cube(int digit1, int count1)
+{
+    while (count1 <= digit1)
+    {
+        Console.WriteLine($"{count1} --> {count1 * count1 * count1,5}");
+        count1++;
+    }
+}
+Console.WriteLine();
+if (digit >= 1)
+{
+    Cube(digit, count);
+}
+else Console.WriteLine($"Введено некорректное число");
