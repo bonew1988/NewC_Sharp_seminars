ec9db71e080a7fab79966bbe9be5ee668326a62b
bonew1988 <bonew@bonew.ru>
2022-09-09 22:12:15 +0300

    add Tasks 14-17 Seminars

---
+++ Task016/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите натуральное число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+double Multiplication(double num)
+{
+    double mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+double result = Multiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} составит {result}");
---
+++ Task017/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+Console.Clear();
+int[] arr = new int[8];
+
+void ArrEight(int[] arr1)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr1[i] = new Random().Next(0, 2);
+    }
+}
+void PrintArray(int[] arr1)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr1[i]},");
+        else
+            Console.Write($"{arr1[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+ArrEight(arr);
+PrintArray(arr);
