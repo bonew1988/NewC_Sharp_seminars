01c51acca831a38b601d890d7951c9a031c93781
bonew1988 <bonew@bonew.ru>
2022-09-13 18:15:55 +0300

    add homework 13.09.2022

---
+++ HomeWorks/13.09.2022/Task027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+//-----------------------------------------------------------------------------------------------
+
+Console.Clear();
+Console.Write("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int accum = 0;
+int count = 0;
+if (num < 0) num = -num;
+int Countingdigits(int num1, int accum1, int i)
+{
+    while (num1 != 0)
+    {
+        accum1 = accum1 + num1 % 10;
+        num1 = num1 / 10;
+        i++;
+    }
+    return accum1;
+}
+int result = Countingdigits(num, accum, count);
+Console.WriteLine("-------------------------");
+Console.WriteLine($"Сумма цифр в введенном числе составит {result}");
---
+++ HomeWorks/13.09.2022/Task029/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+//---------------------------------------------------------------------------------------------
+
+Console.Clear();
+int[] arr1 = new int[8];
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = rnd.Next(0, 999999999);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i == count - 1) Console.WriteLine($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+FillArray(arr1);
+PrintArray(arr1);
+Console.WriteLine();
