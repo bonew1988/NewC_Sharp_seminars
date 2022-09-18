cc279247cf31f9bf5e74ec4e4f8c1e800f7448c6
bonew1988 <bonew@bonew.ru>
2022-09-18 23:43:10 +0300

    homework 19.09.2022 done

---
+++ HomeWorks/19.09.2022/Task043/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y
+// = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Нахождение точки пересечения прямых: ");
+Console.WriteLine("*************************************");
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+void PrintArray(double[] array)
+{
+    Console.Write("(");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}; ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write(")");
+}
+double[] CalcIntersectionPoint(double b11, double k11, double b22, double k22)
+{
+    double x = (b22 - b11) / (k11 - k22);
+    double y = k11 * (b22 - b11) / (k11 - k22) + b11;
+    double[] array = new double[2];
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+double[] arr = CalcIntersectionPoint(b1, k1, b2, k2);
+Console.WriteLine("*************************************");
+Console.Write("Точка пересечения: ");
+PrintArray(arr);
+Console.WriteLine();
